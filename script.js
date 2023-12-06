39c1564ae3b110a0af00dde4b7b59780ba309996
Faizan Manzoor Mir <141254735+FaizanMir01@users.noreply.github.com>
2023-12-06 10:02:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-Shery.mouseFollower();
+
 Shery.makeMagnet(".magnet");
 Shery.hoverWithMediaCircle(".hvr", {
   videos: ["video1.mp4", "video2.mp4", "video3.mp4"],
 });
+
+
+gsap.from(".hvr",{
+  y:200,
+  opacity:0,
+  duration:1,
+  stagger:1
+})
+gsap.from("button",{
+  y:20,
+  duration:.5,
+
+  repeat:-1,
+  yoyo:true
+
+})
+
+
+ var home = document.querySelector("#home");
+
+ var img = document.querySelector(".cursor")
+
+
+ home.addEventListener("mousemove",function(dets){
+  gsap.to(img,{
+    x:dets.x,
+    y:dets.y
+  })
+ })
